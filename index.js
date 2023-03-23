66230b5adf420f949b58abf1e4954589c53c4151
kamala-mohan <69154342+kamala-mohan@users.noreply.github.com>
2023-03-23 23:28:21 +0530

    Setup for basic user schema with pass encryption done

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
-const express  = require("express"); //this pacakge returns a fxn using which we can initiate a new express application object
-const apiRouter = require("./src/routes/index")
-const User = require("./src/models/user")
-const {connect} = require("./src/config/database")
-const app = express(); //exceuting the fxn returned a new express application
+const express = require('express'); // this package returns a function using which we can initiate a new express application object
+const apiRouter = require("./src/routes/index");
+const User = require('./src/models/user');
+const {connect} = require('./src/config/database');
 
+const app = express(); // executing the function returned a new express application
 
-app.use("/api",apiRouter);
-app.get('/',(req,res)=>{
+app.use("/api", apiRouter);
+
+app.get('/', (req, res) => {
     res.status(200);
     res.send({
-        success:true,
-        message:"Successfully hitting the api",
-        data:{}
-       
+        success: true,
+        message: "Succesfully hitting the api",
+        data: {}
     });
-})
+});
 
-app.listen(3000, async ()=>{
-    //this cb will be exceuted everytime the server starts
+app.listen(3000, async () => {
+    // this callback will be executed everytime the server starts
     await connect();
-    console.log("Mongo db Connected Successfully");
-    console.log("Server Started Successfully");
-    let user = await User.create({
-        email:"abc@xyz.com",
-        password:12345,
-        username:"ABC"
-
-     });
-     console.log(user);
+    console.log("Mongo db connected successfully");
+    console.log("Server Started Successsfully");
 });
